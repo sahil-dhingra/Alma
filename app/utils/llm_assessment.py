9b722a8ccb9f5b4a8b0ddf329f91005b3f079746
sahil-dhingra <sahil.dhingra7@gmail.com>
2024-05-31 16:29:53 -0700

    Update llm_assessment.py

--- app/utils/llm_assessment.py
+++ app/utils/llm_assessment.py
@@ -1,14 +1,17 @@
 from app.utils.llm_client import LLMClient
 
-
 class LLMAssessment:
     def __init__(self):
         self.client = LLMClient()
 
     def generate_llm_assessment(self, system_message: str, prompt: str) -> dict:
-        messages = [
-            {"role": "system", "content": system_message},
-            {"role": "user", "content": prompt}
-        ]
-        response = self.client.call_api(messages)
+        if self.client.api_type == "anthropic":
+            messages = [{"role": "user", "content": prompt}]
+            response = self.client.call_api(messages, system=system_message)
+        else:
+            messages = [
+                {"role": "system", "content": system_message},
+                {"role": "user", "content": prompt}
+            ]
+            response = self.client.call_api(messages)
         return response
