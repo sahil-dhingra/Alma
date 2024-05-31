bb78731a4af38271a9b34cdfedbfbf57a7873d12
sahil-dhingra <sahil.dhingra7@gmail.com>
2024-05-31 16:27:51 -0700

    Update llm_client.py

--- app/utils/llm_client.py
+++ app/utils/llm_client.py
@@ -3,6 +3,7 @@ from app.core.config import settings
 from openai import OpenAI
 import openai
 from groq import Groq
+import anthropic
 
 
 class LLMClient:
@@ -10,6 +11,7 @@ class LLMClient:
         self.api_type = settings.API_TYPE
         self.openai_model_name = settings.OPENAI_MODEL_NAME
         self.groq_model_name = settings.GROQ_MODEL_NAME
+        self.anthropic_model_name = settings.ANTHROPIC_MODEL_NAME
         if self.api_type == "openai":
             openai.api_key = settings.OPENAI_API_KEY
             self.client = OpenAI()
@@ -17,6 +19,11 @@ class LLMClient:
             self.api_key = settings.GROQ_API_KEY
             self.client = Groq(
                 api_key=settings.GROQ_API_KEY
+            )
+        elif self.api_type == "anthropic":
+            self.api_key = settings.ANTHROPIC_API_KEY
+            self.client = anthropic.Anthropic(
+                api_key=settings.ANTHROPIC_API_KEY
             )
         else:
             raise ValueError("Invalid API type. Choose either 'openai' or 'groq'.")
@@ -35,10 +42,17 @@ class LLMClient:
             model=self.groq_model_name,
         )
         response = chat_completion.choices[0].message.content
-        if self.is_valid_json(response):
-            return response
-        else:
-            return self.handle_non_json_response(response)
+        return response
+
+    def call_anthropic_client(self, messages, system: str):
+        response = self.client.messages.create(
+            model=self.anthropic_model_name,
+            system=system,
+            max_tokens=2048,
+            temperature=0,
+            messages=messages,
+        )
+        return response
 
     def handle_non_json_response(self, text: str):
         # Generate a prompt to extract JSON from the non-JSON response
@@ -47,7 +61,7 @@ class LLMClient:
             f"{text}"
         )
         extraction_messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are an expert coder that can extract JSON from a text."},
             {"role": "user", "content": extraction_prompt}
         ]
         extracted_json = self.call_groq_client(extraction_messages)
@@ -55,7 +69,8 @@ class LLMClient:
         if self.is_valid_json(extracted_json):
             return json.loads(extracted_json)
         else:
-            return {"error": "Failed to extract valid JSON from the Groq API response"}
+            print({"error": "Failed to extract valid JSON from the Groq API response"})
+            return text
 
     def is_valid_json(self, text: str):
         try:
@@ -64,8 +79,14 @@ class LLMClient:
         except ValueError:
             return False
 
-    def call_api(self, messages):
+    def call_api(self, messages, system=None):
         if self.api_type == "openai":
             return self.call_openai_client(messages)
         elif self.api_type == "groq":
-            return self.call_groq_client(messages)
+            response = self.call_groq_client(messages)
+            if self.is_valid_json(response):
+                return response
+            else:
+                return self.handle_non_json_response(response)
+        elif self.api_type == "anthropic":
+            return self.call_anthropic_client(messages, system)
